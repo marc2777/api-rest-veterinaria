2ede1ed3050e560b731a7710086bac0c8fea64fd
marco <mrcrenton277@gmail.com>
2025-08-24 19:26:29 -0400

    crud empleados,clientes y personas

--- src/clientes.js
+++ src/clientes.js
@@ -7,10 +7,10 @@ router.get("/", async (req, res) => {
   try {
     const [rows] = await pool.query(`
         SELECT p.nombre, p.primer_apellido, p.segundo_apellido,
-               p.telefono, p.direccion, p.email, p.fecha_registro, p.activo,
+               p.telefono, p.direccion, p.email, p.fecha_registro, c.activo,
                c.cliente_id
         FROM personas p
-        INNER JOIN clientes c ON p.persona_id = c.persona_id;
+        INNER JOIN clientes c ON p.persona_id = c.persona_id WHERE c.activo=1;
     `);
     res.json(rows);
   } catch (error) {
@@ -19,6 +19,7 @@ router.get("/", async (req, res) => {
     res.status(500).json({ error: "Error en el servidor" });
   }
 });
+
 
 router.post("/", async (req, res) => {
   try {
@@ -52,12 +53,67 @@ router.post("/", async (req, res) => {
   }
 });
 
-router.put("/:id", (req, res) => {});
+router.put("/:id", async (req, res) => {
+  const { id } = req.params;
+  const campos = req.body;
+  const clientes = ["nombre", "primer_apellido", "segundo_apellido", "telefono", "direccion", "email"];
 
-router.delete("/:id", (req, res) => {
-  //Aqui debe ir codigo de BORRAD LOGICO (active=0) de
-  //la tabla en la que se esta trabajando, NO HACEMOS BORRADO FISICO
-  //OSEA NO USAREMOS EL TIPO DE QUERY "DELETE FROM ...".
+  try {
+    const [rows] = await pool.query(
+      `SELECT persona_id FROM clientes WHERE cliente_id = ?`,
+      [id]
+    );
+
+    if (!rows || rows.length === 0) {
+      return res.json({ mensaje: "Cliente no encontrado." });
+    }
+
+    const personaId = rows[0].persona_id;
+    let columnas = [];
+    let valores = [];
+
+    for (let i = 0; i < clientes.length; i++) {
+      let key = clientes[i];
+
+      if (campos[key] !== undefined && campos[key] !== null) {
+        columnas.push(key);
+        valores.push(campos[key]);
+      }
+    }
+
+    if (columnas.length > 0) {
+      let keyPersonas = columnas.map(k => `${k} = ?`).join(", ");
+      await pool.query(
+        `UPDATE personas SET ${keyPersonas} WHERE persona_id = ?`,
+        [...valores, personaId]
+      );
+      return res.json({ mensaje: "Cliente actualizado exitosamente." });
+    } else {
+      return res.json({ mensaje: "No se enviaron campos para actualizar." });
+    }
+
+  } catch (error) {
+    console.error(`Error al actualizar cliente: ${error}`);
+    res.status(500).json({ mensaje: "Error al actualizar cliente." });
+  }
+});
+
+
+router.delete("/:id", async (req, res) => {
+  try {
+
+    const { id } = req.params;
+
+    await pool.query(`UPDATE clientes SET activo=0 WHERE cliente_id=?`, [id]);
+
+    res.json({ mensaje: "Cliente dado de baja exitosamente." });
+
+  } catch (error) {
+    console.log(`Error al dar de baja al cliente: ${error}`);
+    res.status(500).json({ mensaje: "Error al dar de baja al cliente." });
+    
+  }
+
 });
 
 export default router;
--- src/empleados.js
+++ src/empleados.js
@@ -7,9 +7,9 @@ router.get("/", async (req, res) => {
   try {
     const [rows] =
       await pool.query(`SELECT p.nombre, p.primer_apellido,p.segundo_apellido
-        ,p.telefono,p.direccion,p.email,p.fecha_registro,p.activo,e.empleado_id,c.nombre AS cargo
+        ,p.telefono,p.direccion,p.email,p.fecha_registro,e.activo,e.empleado_id,c.nombre AS cargo
         FROM personas p INNER JOIN empleados e ON p.persona_id = e.persona_id
-        LEFT JOIN cargos c ON e.cargo_id=c.cargo_id; `);
+        LEFT JOIN cargos c ON e.cargo_id=c.cargo_id WHERE e.activo=1; `);
     res.json(rows);
   } catch (error) {
     console.log(`Error al obtener empleados: ${error}`);
@@ -50,8 +50,73 @@ router.post("/", async (req, res) => {
   }
 });
 
-router.put("/:id", (req, res) => {});
+router.put("/:id", async (req, res) => {
+  const { id } = req.params; // empleado_id
+  const campos = req.body;
+  const columnasPersonas = [
+    "nombre",
+    "primer_apellido",
+    "segundo_apellido",
+    "telefono",
+    "direccion",
+    "email",
+  ];
 
-router.delete("/:id", (req, res) => {});
+  try {
+    const [empleado] = await pool.query(
+      "SELECT persona_id FROM empleados WHERE empleado_id = ?",
+      [id]
+    );
+
+    if (!empleado || empleado.length === 0) {
+      return res.json({ mensaje: "Empleado no encontrado..." });
+    }
+
+    const personaId = empleado[0].persona_id;
+
+    let keysPersonas = [];
+    let valoresPersonas = [];
+
+    for (let i = 0; i < columnasPersonas.length; i++) {
+      let key = columnasPersonas[i];
+      if (campos[key] !== undefined && campos[key] !== null) {
+        keysPersonas.push(key);
+        valoresPersonas.push(campos[key]);
+      }
+    }
+
+    if (keysPersonas.length > 0) {
+      let columnas = keysPersonas.map((k) => `${k} = ?`).join(", ");
+      const sql = `UPDATE personas SET ${columnas} WHERE persona_id = ?`;
+      await pool.query(sql, [...valoresPersonas, personaId]);
+    }
+
+    if (campos.cargo !== undefined && campos.cargo !== null) {
+      await pool.query(
+        "UPDATE empleados SET cargo_id = ? WHERE empleado_id = ?",
+        [campos.cargo, id]
+      );
+    }
+
+    res.json({ mensaje: "Empleado actualizado exitosamente." });
+  } catch (error) {
+    console.error(`Error al actualizar empleado: ${error}`);
+    res.status(500).json({ mensaje: "Error al actualizar empleado." });
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    await pool.query(`UPDATE empleados SET activo=0 WHERE empleado_id=?`, [id]);
+
+    res.json({ mensaje: "Empleado dado de BAJA exitosamente." });
+  } catch (error) {
+    console.log(`Error al dar de baja al empleado: ${error}`);
+
+    res.status(500).json({ error: "Error en el servidor" });
+  }
+});
 
 export default router;
